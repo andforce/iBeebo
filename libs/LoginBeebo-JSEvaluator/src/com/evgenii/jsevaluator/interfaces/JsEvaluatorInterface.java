1b221ee0f1fca0a264e63b4faf0b0a45ce881ebe
andforce <86118@163.com>
2015-03-20 12:24:53 +0800

    delete jsevaluator

--- libs/LoginBeebo-JSEvaluator/src/com/evgenii/jsevaluator/interfaces/JsEvaluatorInterface.java
+++
@@ -1,9 +0,0 @@
-package com.evgenii.jsevaluator.interfaces;
-
-public interface JsEvaluatorInterface {
-	public void callFunction(String jsCode, JsCallback resultCallback, String name, Object... args);
-
-	public void evaluate(String jsCode);
-
-	public void evaluate(String jsCode, JsCallback resultCallback);
-}
--- libs/LoginBeebo-JSEvaluator/src/com/evgenii/jsevaluator/interfaces/WebViewWrapperInterface.java
+++
@@ -1,9 +0,0 @@
-package com.evgenii.jsevaluator.interfaces;
-
-import android.webkit.WebView;
-
-public interface WebViewWrapperInterface {
-	public void loadJavaScript(String javascript);
-
-    public WebView getWebView();
-}
