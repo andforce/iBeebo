0c9aa2de06e29608b2e4819f094d6e12d57899ff
andforce <86118@163.com>
2015-03-20 10:55:22 +0800

    delete httputils

--- src/org/zarroboogs/util/net/HttpPostHelper.java
+++ src/org/zarroboogs/util/net/HttpPostHelper.java
@@ -1,41 +1,7 @@
 
 package org.zarroboogs.util.net;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Serializable;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-
-import lib.org.zarroboogs.utils.net.BroserContent;
-import lib.org.zarroboogs.utils.net.HttpFactory;
-import lib.org.zarroboogs.weibo.login.utils.Constaces;
-
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.CookieStore;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.FileEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicNameValuePair;
-import org.zarroboogs.utils.CookieUtils;
-import org.zarroboogs.utils.CourseHeader;
-import org.zarroboogs.utils.CourseUrls;
-import org.zarroboogs.utils.PatternUtils;
-import org.zarroboogs.weibo.bean.UploadPicResult;
-
-import com.google.gson.Gson;
-
-import android.util.Log;
 
 public class HttpPostHelper {
 
